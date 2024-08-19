fc9bc9a97e8836944548fa1d7988c3a5c9a87cb3
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2024-08-19 16:44:59 +0900

    feat: user, postLike, tag, postTag, comment 모델 생성

---
+++ src/models/postTag.model.ts
@@ -0,0 +1,52 @@
+import { DataTypes, Model, Optional, Sequelize } from "sequelize";
+
+interface PostTagAttributes {
+  postId: number;
+  tagId: number;
+  createdAt: Date;
+}
+
+interface PostTagCreationAttributes
+  extends Optional<PostTagAttributes, "createdAt"> {}
+
+class PostTag
+  extends Model<PostTagAttributes, PostTagCreationAttributes>
+  implements PostTagAttributes
+{
+  public postId!: number;
+  public tagId!: number;
+  public readonly createdAt!: Date;
+
+  static initModel(sequelize: Sequelize) {
+    PostTag.init(
+      {
+        postId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: { model: "posts", key: "id" },
+        },
+        tagId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: { model: "tags", key: "id" },
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: DataTypes.NOW,
+        },
+      },
+      {
+        sequelize,
+        modelName: "PostTag",
+        tableName: "post_tags",
+        charset: "utf8",
+        collate: "utf8_general_ci",
+        timestamps: false,
+        underscored: true,
+      }
+    );
+  }
+}
+
+export default PostTag;
